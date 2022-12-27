2c5d4ea4a447c9269744822fd6d6ffa93d821ad4
VicRoger27 <121492850+VicRoger27@users.noreply.github.com>
2022-12-27 11:35:19 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (Math.randomBoolean()) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
