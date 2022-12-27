475af7c1766315fd72c8e2b3cf8f61ad42e1c3e2
VicRoger27 <121492850+VicRoger27@users.noreply.github.com>
2022-12-27 11:44:24 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,9 @@ basic.forever(function () {
             # # # # #
             # # # # #
             `)
+        for (let index = 0; index < 2; index++) {
+            music.playMelody("C5 C A C B C F C ", 218)
+        }
     } else {
         basic.showLeds(`
             . . . . .
@@ -15,5 +18,16 @@ basic.forever(function () {
             . . . . .
             . . . . .
             `)
+        for (let index = 0; index < 2; index++) {
+            music.playMelody("F E D E C D F C ", 82)
+        }
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        game.addScore(100000000)
+        game.gameOver()
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        game.setScore(1000000000)
+        game.gameOver()
     }
 })
